89ad06e644ed07ff444b790a8dddbad1bbfc8120
Guillaume Poirrier <guillaume@twelveparsecs.co>
2025-10-01 17:06:53 +0200

    (fix): ci pipeline

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DinoGame from "react-chrome-dino";
+
+function App() {
+    return (
+        <div>
+            <h1>TP04 – Dino Game</h1>
+            <DinoGame />
+        </div>
+    );
+}
+
+export default App;
